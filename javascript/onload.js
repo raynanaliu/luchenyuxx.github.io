0996f6668517cf4d5e4837ccf036ba94f2bf75ca
chenyu <luchenyuxx@gmail.com>
2016-12-30 18:32:55 +0100

    modify contact.html, change foot and spot cursor style. click foot to go index.

--- javascript/onload.js
+++ javascript/onload.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $("body").on("contextmenu", function(e) {
         return false;
     });
+    $("#foot").click(function () {
+        window.location.href="index.html"
+    })
 });
