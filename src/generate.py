095dd3ecbb1c88ccb351acaef3d335a3e958c4d2
luchenyuxx <chenyu.lu@density-tech.com>
2019-02-17 16:03:29 +0100

    correct year order in menu. generate html from template in src, see src/generate.py

---
+++ src/generate.py
@@ -0,0 +1,20 @@
+import pystache
+
+TEMPLATE = open("template.html", "r").read()
+
+def generate(file_name):
+    content = open(file_name, "r").read()
+    output = pystache.render(TEMPLATE, {'content': content})
+    open("../" + file_name, "w").write(output)
+
+generate("apple.html")
+generate("blue_ant.html")
+generate("contact.html")
+generate("ear.html")
+generate("fashion.html")
+generate("index.html")
+generate("no_star.html")
+generate("portfolio_set.html")
+generate("runaway_comma.html")
+generate("silver_universe.html")
+generate("sweet_nonsense.html")
